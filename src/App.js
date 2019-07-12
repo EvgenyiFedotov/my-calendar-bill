5e668aeb409e77066b4750288b38123a7bb15c19
e.fedotov <fedotov1290@gmail.com>
2019-07-12 12:32:30 +0300

    delete `ListBills`

--- src/App.js
+++ src/App.js
@@ -10,27 +10,37 @@ import EditDialog from './components/core/EditDialog';
 import ModalWindow from './components/core/ModalWindow';
 import List from './components/core/List';
 import LabelText from './components/core/LabelText';
+import Branch from './components/core/Branch';
 
 function App() {
   const [date, setDate] = React.useState(new Date());
+  const [showDialog, setShowDialog] = React.useState(false);
 
   return (
     <AppContext>
       <AppGlobalStyled theme="white" />
       <AppStyled>
         <TriggerMonth date={date} onChangeDate={setDate} />
-        <Calendar date={date} />
-        <ModalWindow>
-          <EditDialog>
-            <List
-              items={new Map([[1, 1], [2, 2], [3, 3]])}
-              getItemProps={([, value]) => ({
-                children: <LabelText>{value}</LabelText>,
-                justifyContent: 'flex-end',
-              })}
-            />
-          </EditDialog>
-        </ModalWindow>
+        <Calendar
+          date={date}
+          getDateProps={() => ({
+            onClick: () => setShowDialog(true),
+          })}
+        />
+
+        <Branch value={showDialog}>
+          <ModalWindow onClose={() => setShowDialog(false)}>
+            <EditDialog onCancel={() => setShowDialog(false)}>
+              <List
+                items={new Map([[1, 1], [2, 2], [3, 3]])}
+                getItemProps={([, value]) => ({
+                  children: <LabelText>{value}</LabelText>,
+                  justifyContent: 'flex-end',
+                })}
+              />
+            </EditDialog>
+          </ModalWindow>
+        </Branch>
       </AppStyled>
     </AppContext>
   );
