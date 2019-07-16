4a85b66177315c93b1f1ac909db9d92562f8c3b4
e.fedotov <fedotov1290@gmail.com>
2019-07-16 21:34:22 +0300

    create `use-stepper-date`

--- src/components/subjects/contexts/App/index.jsx
+++ src/components/subjects/contexts/App/index.jsx
@@ -7,15 +7,17 @@ import Context from './context';
 import useChangesBill from './use-changes-bill';
 import useCheckList from './use-check-list';
 import useTheme from './use-theme';
+import useStepperDate from './use-stepper-date';
 
 const App = ({ children }) => {
   const date = useDate();
+  const stepperDate = useStepperDate(date);
   const changesBill = useChangesBill();
   const checkList = useCheckList(changesBill);
   const theme = useTheme();
 
   return (
-    <Context.Provider value={{ date, changesBill, checkList, theme }}>
+    <Context.Provider value={{ date, stepperDate, changesBill, checkList, theme }}>
       <AppGlobalStyled theme={theme[0]} />
       {children}
     </Context.Provider>
