8d3b0f51c02ecbf01082fcee4ae601f95746773d
e.fedotov <fedotov1290@gmail.com>
2019-07-18 21:09:56 +0300

    remove handler item in `use-list-idb`

--- src/hooks/use-list-idb.js
+++ src/hooks/use-list-idb.js
@@ -5,13 +5,9 @@ import { methodsTable } from 'helpers/index-db';
 /**
  * @param {Promise<IndexDB>} db
  * @param {string} nameTable
- * @param {Object} [options = {}]
- * @param {(item: Object) => any} [options.stringifyItem]
- * @param {(item: Object) => any} [options.parseItem]
  */
-export default (list, db, nameTable, options = {}) => {
+export default (list, db, nameTable) => {
   const { saveItem, setItems, deleteItem } = list;
-  const { stringifyItem, parseItem } = options;
   const table = React.useMemo(() => methodsTable(db, nameTable), [db, nameTable]);
 
   /**
@@ -22,9 +18,9 @@ export default (list, db, nameTable, options = {}) => {
   const saveItemIdb = React.useCallback(
     (newItemProps, key) => {
       const [normKey, normItem] = saveItem(newItemProps, key);
-      return table.set(normKey, stringifyItem ? stringifyItem(normItem) : JSON.stringify(normItem));
+      return table.set(normKey, JSON.stringify(normItem));
     },
-    [saveItem, table, stringifyItem],
+    [saveItem, table],
   );
 
   /**
@@ -43,13 +39,13 @@ export default (list, db, nameTable, options = {}) => {
     () => table
       .getMap((item) => {
         try {
-          return parseItem ? parseItem(item) : JSON.parse(item);
+          return JSON.parse(item);
         } catch (e) {
           return {};
         }
       })
       .then(items => setItems(items)),
-    [table, setItems, parseItem],
+    [table, setItems],
   );
 
   return {
